10992a3b876584efebe65523fd81125cf78368e4
你手捧鲜花 <157215725@qq.com>
2021-09-10 11:14:13 +0800

    refactor(input): style

--- packages/veno-ui/src/components/ve-input/VeInput.tsx
+++ packages/veno-ui/src/components/ve-input/VeInput.tsx
@@ -3,18 +3,25 @@ import './styles/ve-input.scss'
 import { defineComponent } from 'vue'
 
 import { makeBorderProps, useBorder } from '../../composables/border'
+import { makeDisabledProps, useDisabled } from '../../composables/disabled'
 import { makeVariantProps, useVariant } from '../../composables/variant'
 
 export default defineComponent({
   name: 'VeInput',
 
   props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
     modelValue: [String, Number],
+    ...makeDisabledProps(),
     ...makeBorderProps(),
     ...makeVariantProps(),
   },
 
   setup (props, { slots, emit }) {
+    const { disabledClasses } = useDisabled(props, 've-input')
     const { borderClasses } = useBorder(props, 've-input')
     const { colorClasses, colorStyles, variantClasses } = useVariant(props, 've-input')
 
@@ -24,6 +31,7 @@ export default defineComponent({
           class={ [
             've-input',
             colorClasses.value,
+            disabledClasses.value,
             borderClasses.value,
             variantClasses.value,
           ] }
@@ -32,17 +40,30 @@ export default defineComponent({
           ] }
         >
           {
-            slots.prepend && (
-              <div class="ve-input__prepend">
-                { slots.prepend?.() }
+            slots.prefix && (
+              <div class="ve-input__prefix">
+                { slots.prefix?.() }
               </div>
             )
           }
 
-          <input
-            value={ props.modelValue }
-            onInput={ e => emit('update:modelValue', (e.target as HTMLInputElement).value) }
-          />
+          <div class="ve-input__input">
+            <input
+              class="ve-input__input-el"
+              value={ props.modelValue }
+              type={ props.type }
+              onInput={ e => emit('update:modelValue', (e.target as HTMLInputElement).value) }
+              disabled={ props.disabled }
+            />
+          </div>
+
+          {
+            slots.suffix && (
+              <div class="ve-input__suffix">
+                { slots.suffix?.() }
+              </div>
+            )
+          }
         </div>
       )
     }
