5839d190f9d4a66f831984a208f0513e08c6536c
你手捧鲜花 <157215725@qq.com>
2022-03-30 13:34:20 +0800

    docs: fix lang load

--- packages/docs/src/plugins/veno-ui.ts
+++ packages/docs/src/plugins/veno-ui.ts
@@ -7,8 +7,20 @@ import * as directives from 'veno-ui/directives'
 import * as providers from 'veno-ui/providers'
 import aliases from '~veno-ui/icons'
 
-// highlighter
+// Highlighter
 import { getHighlighter, setCDN } from 'shiki'
+
+// Themes
+import themeMaterialPalenight from 'shiki/themes/material-palenight.json'
+
+// Languages
+import langHTML from 'shiki/languages/html.tmLanguage.json'
+import langJS from 'shiki/languages/javascript.tmLanguage.json'
+import langTS from 'shiki/languages/typescript.tmLanguage.json'
+import langJson from 'shiki/languages/json.tmLanguage.json'
+import langShell from 'shiki/languages/shellscript.tmLanguage.json'
+import langVueHtml from 'shiki/languages/vue-html.tmLanguage.json'
+import langVue from 'shiki/languages/vue.tmLanguage.json'
 
 // Types
 import type { InstallPlugin } from '@/types'
@@ -25,8 +37,47 @@ export const install: InstallPlugin = ({ app }) => {
     highlighter: {
       type: 'shiki',
       shiki: getHighlighter({
-        theme: 'material-palenight',
-        langs: ['html', 'vue', 'vue-html', 'ts', 'js', 'json', 'shell'],
+        theme: themeMaterialPalenight as any,
+        langs: [
+          {
+            id: 'html',
+            scopeName: langHTML.scopeName,
+            grammar: langHTML as any,
+          },
+          {
+            id: 'javascript',
+            scopeName: langJS.scopeName,
+            grammar: langJS as any,
+            aliases: ['js']
+          },
+          {
+            id: 'typescript',
+            scopeName: langTS.scopeName,
+            grammar: langTS as any,
+            aliases: ['ts']
+          },
+          {
+            id: 'vue',
+            scopeName: langVue.scopeName,
+            grammar: langVue as any,
+          },
+          {
+            id: 'vue-html',
+            scopeName: langVueHtml.scopeName,
+            grammar: langVueHtml as any,
+          },
+          {
+            id: 'json',
+            scopeName: langJson.scopeName,
+            grammar: langJson as any,
+          },
+          {
+            id: 'shell',
+            scopeName: langShell.scopeName,
+            grammar: langShell as any,
+            aliases: ['sh'],
+          },
+        ],
       }),
     },
   })
